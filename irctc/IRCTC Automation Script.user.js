faed118628f0a1789be3c8beea1a02854a9559d4
Amritanshu Singh <amritanshu.singh@gmail.com>
2015-11-13 19:26:07 +0530

    Initial barebones structure which bingos (commented out) the google search page as a starter

---
+++ irctc/IRCTC Automation Script.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         IRCTC Automation Script
+// @namespace    http://www.amritanshusingh.com/
+// @version      0.1
+// @description  Automating the terribly painful task of booking IRCTC tickets.
+// @author       Amritanshu Singh
+// @match        https://www.google.co.in/*
+// @grant        2015+, amritanshusingh.com
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+$(document).ready(function(){
+    //$("#lst-ib").val("Tame the monkey");
+    //alert('bingo');
+});
